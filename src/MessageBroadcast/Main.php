bc0024f5acf5800bb995d13ef77d514169090e11
applqpak <applqpak@gmail.com>
2016-01-30 14:12:05 +1100

    Create Main.php

---
+++ src/MessageBroadcast/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+  namespace MessageBroadcast;
+
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+  use pocketmine\command\CommandExecutor;
+
+  class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onComamnd(CommandSender $sender, Command $cmd, $label, array $args) {
+
+      if(strtolower($cmd->getName()) === "broadcast") {
+
+        if(!(isset($args[0]))) {
+
+          $sender->sendMessage(TF::RED . "Error: not enough args!");
+
+        } else {
+
+          $message = $args[0];
+
+          $this->getServer()->broadcastMessage($message);
+          $sender->sendMessage(TF::GREEN . "Successfully broadcasted message!");
+
+        }
+
+      }
+
+    }
+
+  }
+
+?>
